23d7ec1812112310715f20a7eeb77b5ad55b9c61
Marga <margalppalacios@gmail.com>
2022-04-01 10:17:29 +0200

    Versión final

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import matplotlib.pyplot as plt
 
 arr = [[6,6], [-1,8], [9,1], [6,-5]]
 
-def coordenadas(lista):
+def maximo_abs(lista):
+    max_coord =abs(max(max(lista)))
+    min_coord =abs(min(min(lista)))
 
+    return max(max_coord, min_coord)
+
+
+def coordenadas(lista):
+    max_coord = maximo_abs(arr)+1
     cont = 0
+    plt.plot(0,0,'k+')
     
     while(cont < len(lista)-1):
+
+        plt.xlim(-1*max_coord, max_coord)
+        plt.ylim(-1* max_coord, max_coord)
+
         for coord in lista:
             x = lista[cont][0]
             y = lista[cont][1]
@@ -14,7 +26,7 @@ def coordenadas(lista):
 
 
             plt.scatter(x, y)
-            plt.pause(1.00)
+            plt.pause(0.50)
     
     
     plt.legend(lista)
